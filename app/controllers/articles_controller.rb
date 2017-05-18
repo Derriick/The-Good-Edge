ef462474c7750b8deadfe9782bf9b9d2f94bb3fb
Derriick <derriick.ensiie@yahoo.com>
2017-05-19 01:46:24 +0200

    Edition utilisateur et (presque) article
    on ne peut pas encore vérifier si un article appartient bien à celui qui essaye de le modifier

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.create(params.require(:article).permit(:titre, :description, :prix))
+		@article = Article.create(params.require(:article).permit(:titre, :prix, :description))
 
 		redirect_to @article
 	end
@@ -20,6 +20,15 @@ class ArticlesController < ApplicationController
 	def edit
 		if session[:utilisateur_id] != @article.utilisateur.id
 			redirect_to(utilisateurs_path, notice: "Vous n'êtes pas autorisés à modifier ce profil")
+		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update_attributes(params.require(:article).permit(:titre, :prix, :description))
+			redirect_to @article
+		else
+			render :edit
 		end
 	end
 
--- app/controllers/utilisateurs_controller.rb
+++ app/controllers/utilisateurs_controller.rb
@@ -19,6 +19,14 @@ class UtilisateursController < ApplicationController
 		if @utilisateur != Utilisateur.find(params[:id])
 			redirect_to(articles_path, notice: "Vous n'êtes pas autorisés à modifier ce profil")
 		end
+	end
 
+	def update
+		@utilisateur = Utilisateur.find(params[:id])
+		if @utilisateur.update_attributes(params.require(:utilisateur).permit(:nom, :prenom, :promotion, :email, :telephone))
+			redirect_to @utilisateur
+		else
+			render :edit
+		end
 	end
 end
