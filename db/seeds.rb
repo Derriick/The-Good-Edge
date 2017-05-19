29d8ae5435fdf49080a0e4bbdc5eb8830fe275fd
Damien Lo Monaco <damien-lo@hotmail.fr>
2017-05-19 17:22:46 +0200

    end of egi

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Utilisateur.create({ nom: 'Skywalker', prenom: 'Luke', email: 'empire@contratak.fr', promotion: 2019, telephone: 666 })
-Utilisateur.create({ nom: 'Dieu', prenom: 'Donné', email: 'superconnard@quenelle.fr', promotion: 7, telephone: 123 })
+Utilisateur.create({ nom: 'Skywalker', prenom: 'Luke', email: 'empire@contratak.fr', password: 'bitedure', promotion: 2019, telephone: 666 })
+Utilisateur.create({ nom: 'Dieu', prenom: 'Donné', email: 'superconnard@quenelle.fr', password: 'lepenforever', promotion: 7, telephone: 123 })
 
 Article.create({ titre: 'sextoy', description: 'objet de desir', prix: 14, utilisateur_id: 1 })
 Article.create({ titre: 'voiture', description: 'objet de desir qui roule', prix: 888, utilisateur_id: 2 })
